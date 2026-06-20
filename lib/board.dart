0f014a7e515b26ca7de9c87ac16c96b8bc67b608
Bela <brstamenkovits@gmail.com>
2026-06-20 16:35:15 +0200

    draggable pieces

--- lib/board.dart
+++ lib/board.dart
@@ -10,16 +10,50 @@ class Board extends StatefulWidget {
 
 class _BoardState extends State<Board> {
   final List<PieceData> pieces = [
-    PieceData(id: "piece_11"),
-    PieceData(id: "piece_2"),
-    PieceData(id: "piece_3"),
-    PieceData(id: "piece_4"),
+    PieceData(id: "piece_1", position: const Offset(50, 50)),
+    PieceData(id: "piece_2", position: const Offset(200, 50)),
+    PieceData(id: "piece_3", position: const Offset(50, 200)),
+    PieceData(id: "piece_4", position: const Offset(200, 200)),
   ];
 
   @override
   Widget build(BuildContext context) {
-    return Wrap(
-      children: pieces.map((data) => Piece(data: data)).toList(),
+    /// "camera"
+    return InteractiveViewer(
+      minScale: 0.5,
+      maxScale: 2.0,
+      boundaryMargin: const EdgeInsets.all(1000),
+
+      /// "canvas"
+      child: Container(
+        width: 2000,
+        height: 2000,
+        color: Colors.black,
+
+        /// Puzzle Pieces
+        child: Stack(
+          children: pieces.map((data) {
+            return Positioned(
+              /// Position of the piece inside of the "canvas"
+              left: data.position.dx,
+              top: data.position.dy,
+
+              /// setState method rebuilds the UI
+              /// this updates left and top attribute of the Positioned widget
+              child: Piece(
+                data: data,
+                /// callback function that gets executed whenever a "drag" input
+                /// is detected
+                onDrag: (delta) {
+                  setState(() {
+                    data.position += delta;
+                  });
+                },
+              ),
+            );
+          }).toList(),
+        ),
+      ),
     );
   }
 }
--- lib/piece.dart
+++ lib/piece.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 
 /// Data model for a jigsaw puzzle piece.
 class PieceData {
-
   /// Unique identifier for the piece.
   final String id;
+  /// Current position of the piece on the board.
+  Offset position;
 
-  PieceData({required this.id});
+  PieceData({required this.id, this.position = Offset.zero});
 }
 
 
@@ -15,17 +16,26 @@ class Piece extends StatelessWidget {
   /// The data associated with this piece.
   final PieceData data;
 
-  const Piece({super.key, required this.data});
+  /// Callback when the piece is dragged.
+  final Function(Offset delta) onDrag;
+
+  const Piece({super.key, required this.data, required this.onDrag});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 100,
-      height: 100,
-      margin: const EdgeInsets.all(8),
-      color: Colors.grey,
-      child: Center(
-        child: Text(data.id),
+    return GestureDetector(
+      /// when user performs a pan input, call the onDrag method
+      /// (method defined at class instantiation)
+      onPanUpdate: (details) => onDrag(details.delta),
+
+      /// Visual representation of puzzle piece
+      child: Container(
+        width: 100,
+        height: 100,
+        color: Colors.grey,
+        child: Center(
+          child: Text(data.id),
+        ),
       ),
     );
   }
